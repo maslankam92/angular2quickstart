95ce5500a51c944c049d4270d55775dc031579e1
Marcin Maslanka <maslankam92@gmail.com>
2016-02-26 12:03:20 +0100

    First commit

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by MarcinM on 26-Feb-16.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My second Angular 2 App</h1>'
+})
+
+export class AppComponent {}
+
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by MarcinM on 26-Feb-16.
+ */
+import {bootstrap} from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
