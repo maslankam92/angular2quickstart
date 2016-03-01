b6e2952d67ee8d9eecc09430b7995a13615623cf
Marcin Maslanka <maslankam92@gmail.com>
2016-03-01 21:27:29 +0100

    finished chapter 5

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -2,14 +2,20 @@
 /**
  * Created by MarcinM on 26-Feb-16.
  */
-import {Injectable} from "angular2/core";
-import {HEROES} from "./mock-heroes";
+import {Injectable} from 'angular2/core';
+import {HEROES} from './mock-heroes';
 import {Hero} from './hero';
 
 @Injectable()
 export class HeroService {
     getHeroes(){
         return Promise.resolve(HEROES);
+    }
+
+    getHero(id: number){
+        return Promise.resolve(HEROES).then(
+            heroes => heroes.filter(hero => hero.id === id)[0]
+        );
     }
 //    getHeroesSlowly(){
 //        return new Promise<Hero[]>(resolve =>
